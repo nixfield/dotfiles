be0826d89487ad3587d7b656c560084bcbcc2cd2
niko yama <nikoyama72@gmail.com>
2021-07-16 19:18:16 +0700

    add lv-config

---
+++ lv-config.lua
@@ -0,0 +1,102 @@
+--[[
+O is the global options object
+
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+
+-- general
+
+O.format_on_save = true
+O.completion.autocomplete = true
+O.colorscheme = "spacegray"
+O.auto_close_tree = 0
+O.default_options.wrap = true
+O.default_options.timeoutlen = 100
+O.leader_key = " "
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+O.plugin.dashboard.active = true
+O.plugin.zen.active = false
+O.plugin.zen.window.height = 0.90
+
+-- if you don't want all the parsers change this to a table of the ones you want
+O.treesitter.ensure_installed = "maintained"
+O.treesitter.ignore_install = { "haskell" }
+O.treesitter.highlight.enabled = true
+
+-- python
+-- O.lang.python.linter = 'flake8'
+O.lang.python.isort = true
+O.lang.python.diagnostics.virtual_text = true
+O.lang.python.analysis.use_library_code_types = true
+-- to change default formatter from yapf to black
+-- O.lang.python.formatter.exe = "black"
+-- O.lang.python.formatter.args = {"-"}
+
+-- go
+-- to change default formatter from gofmt to goimports
+-- O.lang.formatter.go.exe = "goimports"
+
+-- javascript
+O.lang.tsserver.linter = nil
+
+-- rust
+-- O.lang.rust.formatter = {
+--   exe = "rustfmt",
+--   args = {"--emit=stdout", "--edition=2018"},
+-- }
+
+-- latex
+-- O.lang.latex.auto_save = false
+-- O.lang.latex.ignore_errors = { }
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- O.user_autocommands = {{ "BufWinEnter", "*", "echo \"hi again\""}}
+
+-- Additional Leader bindings for WhichKey
+-- O.user_which_key = {
+--   A = {
+--     name = "+Custom Leader Keys",
+--     a = { "<cmd>echo 'first custom command'<cr>", "Description for a" },
+--     b = { "<cmd>echo 'second custom command'<cr>", "Description for b" },
+--   },
+-- }
+
+-- Additional Plugins
+O.user_plugins = {
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "*" }, {
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+			})
+		end,
+	},
+	{ "vimwiki/vimwiki"},
+	{
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		ft = "markdown",
+	},
+}
+
+-- keymappings
+vim.api.nvim_set_keymap("n", "j", "gj", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "k", "gk", { noremap = true, silent = true })
+
+-- vim wiki config
+vim.g.vimwiki_list = {
+	{ path = "/$HOME/Vimwiki", syntax = "markdown", ext = ".md" },
+	{ path = "/$HOME/Vimwiki/project/akb", syntax = "markdown", ext = ".md" },
+}
